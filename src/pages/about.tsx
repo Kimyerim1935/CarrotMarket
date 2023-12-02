d27b811f18723a8f9d3164f8b30068b9bcef09f8
김예림 <61038091+Kimyerim1935@users.noreply.github.com>
2023-12-02 23:51:54 +0900

    add: global css 설정 및 레이아웃(헤더) 컴포넌트

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,6 @@ import NavBar from "@/components/NavBar";
 
 export default function About() {
   return (
-    <div>
-            <NavBar/>hello About</div>
+    <div>hello About</div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import NavBar from '@/components/NavBar'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <div>
-      <NavBar/>
+      this is home
     </div>
   )
 }
